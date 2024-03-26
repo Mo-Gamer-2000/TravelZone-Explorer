eb53306bcbfab6fb665e425df61e7992db982798
Moeez Abdul <moeezrajput2000@gmail.com>
2024-03-26 15:45:30 +0000

    Add project files.

---
+++ TravelZone/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TravelZone.Areas.Data;
+
+namespace TravelZone.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ TravelZone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelZone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
